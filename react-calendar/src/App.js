e62da0497f0f46d856559ab477dc61751c2d2b66
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-19 13:49:44 -0700

    small changes

--- react-calendar/src/App.js
+++ react-calendar/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Calendar from './components/Calendar';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <br />
+            <span>
+              react <b>calendar</b>
+            </span>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <Calendar />
+        </main>
       </div>
     );
   }
