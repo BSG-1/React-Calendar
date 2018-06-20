775ac87afa729dc0ade9840b028c6eb5a4e36a2b
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-19 21:07:31 -0700

    finishes out renderDays()

--- react-calendar/src/components/Calendar.jsx
+++ react-calendar/src/components/Calendar.jsx
@@ -42,11 +42,12 @@ class Calendar extends React.Component {
         for (let i = 0; i < 7; i++) {
             days.push(
                 <div className="col col-center" key={i}>
-
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
                 </div>
-
-            )
+            );
         }
+
+        return <div className="days row">{days}</div>
     };
 
     renderCells() {
