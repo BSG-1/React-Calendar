146f0a7e82cf4882a4a267b4c0b707bea5ded9a8
Ghataorhe <b.ghataorhe@gmail.com>
2018-06-19 20:42:12 -0700

    adds state and render methods to Calender.jsx

--- react-calendar/src/components/Calendar.jsx
+++ react-calendar/src/components/Calendar.jsx
@@ -2,6 +2,23 @@ import React from "react";
 import dateFns from "date-fns";
 
 class Calendar extends React.Component {
+    state = {
+        currentMonth: new Date(),
+        selectedMonth: new Date()
+    };
+
+    renderHeader() { }
+
+    renderDays() { }
+
+    renderCells() { }
+
+    onDateClick = day => { };
+
+    nextMonth = () => { };
+
+    prevMonth = () => { };
+
     render() {
         return (
             <div>
